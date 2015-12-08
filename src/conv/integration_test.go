af449aad726c48c559c0357fbd7e922c2a2c31d1
Martin Ellison <m.e@acm.org>
2015-12-08 18:36:11 +0800

    add integration test

---
+++ src/conv/integration_test.go
@@ -0,0 +1,40 @@
+// integration_test.go
+package main
+
+// Copyright 2015 Martin Ellison. For GPL3 licence notice, see the end of this file.
+// parser_test.go
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntegration1(t *testing.T) {
+	testIntegration([]string{"$[page fred]$"}, "pages = {  fred: { init: function() {  }, display: function(parts) {  }, fix: function(parts) {  }, },  };", t)
+}
+func testIntegration(inLines []string, expected string, t *testing.T) {
+	assert := assert.New(t)
+	lineChan = make(chan scanLine)
+	tokenChan = make(chan token)
+	var theTokeniser tokeniser
+	theTokeniser.init()
+	go theTokeniser.getTokens()
+	theMockFileReader := makeMockFileReader(inLines)
+	go sendInlines(theMockFileReader, assert)
+	theParseChan := make(chan *pageSet)
+	var theParser parser
+	go theParser.parse(theParseChan)
+	thePageSet := <-theParseChan
+	var theGenerator generator
+	//	theGenerator.init()
+	theGenerator.makeTemplate()
+	var theOutData outData
+	theOutData.makeGenData(thePageSet)
+	testBuffer := new(bytes.Buffer)
+	theGenerator.expandTemplate(testBuffer, theOutData)
+	assert.Equal(expected, testBuffer.String())
+}
+
+// This file is part of OldRope. OldRope is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. OldRope is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OldRope. If not, see <http://www.gnu.org/licenses/>.
--- src/conv/tokens_test.go
+++ src/conv/tokens_test.go
@@ -8,45 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockFileReader struct {
-	inlines   []string
-	nextIndex int
-}
-
-func makeMockFileReader(theInlines []string) *mockFileReader {
-	return &mockFileReader{inlines: theInlines}
-}
-func (theMockFileReader *mockFileReader) getLine(assert *assert.Assertions) (theLine string, eof bool) {
-	numLines := len(theMockFileReader.inlines)
-	assert.True(theMockFileReader.nextIndex < numLines+1)
-	if theMockFileReader.nextIndex == numLines {
-		theMockFileReader.nextIndex++
-		theLine = ""
-		eof = true
-		//	log.Print("end of mock input")
-		return
-	}
-	//log.Printf("using mock line %d of %d", theMockFileReader.nextIndex, numLines)
-	theLine = theMockFileReader.inlines[theMockFileReader.nextIndex]
-	theMockFileReader.nextIndex++
-	return
-}
-func (theMockFileReader *mockFileReader) verify(assert *assert.Assertions) {
-	assert.True(theMockFileReader.nextIndex == len(theMockFileReader.inlines)+1)
-}
-func sendInlines(theMockFileReader *mockFileReader, assert *assert.Assertions) {
-	//logging = true
-	theLine, eof := theMockFileReader.getLine(assert)
-	lineNumber := 1
-	for !eof {
-		//log.Printf("scan line %d", lineNumber)
-		lineChan <- scanLine{text: theLine, number: lineNumber, eof: false}
-		theLine, eof = theMockFileReader.getLine(assert)
-		lineNumber++
-	}
-	//log.Print("end of scan lines")
-	lineChan <- scanLine{text: "", number: lineNumber, eof: true}
-}
 func TestConv1(t *testing.T) {
 	assert := assert.New(t)
 	lineChan = make(chan scanLine)
