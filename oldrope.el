deaa5bfb2d5feb01f2036d1a0cfce1c453eb2dc4
Martin Ellison <m.e@acm.org>
2015-12-18 11:23:40 +0800

    fix emacs mode per https://stackoverflow.com/questions/34192831/my-emacs-mode-does-not-highlight-keywords

--- oldrope.el
+++ oldrope.el
@@ -1,8 +1,8 @@
 ;; emacs mode for OldRope
 ;; does no work for some reason
 (setq oldrope-directive-keywords-regexp (regexp-opt  '("page" "link" "goto" "act" "end" "div" "span" "include")))
-(defvar oldrope-font-lock-defaults  '((
-				       (oldrope-directive-keywords-regexp . font-lock-keyword-face))))
+ (defvar oldrope-font-lock-defaults
+         `(((,oldrope-directive-keywords-regexp . font-lock-keyword-face))))
 (define-derived-mode oldrope-mode fundamental-mode
   "oldrope mode"
   "Major mode for editing OldRope games"
